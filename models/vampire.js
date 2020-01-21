25b5da1149ebc3d392a712642064b1f9f314a961
ahmadalarif <44748381+ahmadalarif@users.noreply.github.com>
2020-01-21 21:49:26 +0300

    Lap+Hw

--- models/vampire.js
+++ models/vampire.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
-  // add your code here to set up your schema
+  // Building a Schema
+  name:{ type: String , required: true},
+  title: String,
+  hair_color: { type: String , default: "blonde"},
+  eye_color: String,
+  dob: Date,
+  loves: [String],
+  location: String,
+  gender: Number,
+  victims: { type: Number , default: 2}
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
